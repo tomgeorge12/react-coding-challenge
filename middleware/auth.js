e0f262e8a2417a9f6b828995ae981670081476df
tomgeorge12 <tom.george@metlife.com>
2019-11-13 15:21:13 +0900

    auth added

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,18 @@
-const config = require('config');
-const jwt = require('jsonwebtoken');
+// const config = require('config');
+// const jwt = require('jsonwebtoken');
 
 function auth(req, res, next) {
   const token = req.header('x-auth-token');
 
   // Check for token
-  if (!token)
-    return res.status(401).json({ msg: 'No token, authorizaton denied' });
+//   if (!token)
+//     return res.status(401).json({ msg: 'No token, authorizaton denied' });
 
   try {
     // Verify token
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    // const decoded = jwt.verify(token, config.get('jwtSecret'));
     // Add user from payload
-    req.user = decoded;
+    // req.user = decoded;
     next();
   } catch (e) {
     res.status(400).json({ msg: 'Token is not valid' });
